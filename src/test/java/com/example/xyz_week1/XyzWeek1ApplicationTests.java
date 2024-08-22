9e96d2d8a0b06572bee0399181220571ff3aa805
Glcan22 <103893419+Glcan22@users.noreply.github.com>
2024-08-22 16:28:57 +0300

    Initial commit: Spring Boot project setup for XYZ application

---
+++ src/test/java/com/example/xyz_week1/XyzWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xyz_week1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
